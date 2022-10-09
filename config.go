5eb7ef85c0aedb7b2fd9b1058707979e74f4e14e
Igor Lourenço <igorlourenco.dev@gmail.com>
2022-10-09 12:36:37 -0300

    feat: init config to connect to database

---
+++ config.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+/*
+ * this is the struct that will contain the allowed configurations
+ * port number and postgresql connection string
+ */
+type Config struct {
+	Port            string `mapstructure:"port"`
+	ConnectionSting string `mapstructure:"connection_string"`
+}
+
+// this is the global variable that will be used to access the configurations
+var AppConfig *Config
+
+func loadAppConfig() {
+
+	// using viper to load the configurations from the config.json file
+	log.Println("Loading server configurations...")
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = viper.Unmarshal(&AppConfig)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
